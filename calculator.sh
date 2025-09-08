1781fff4f3d71f19098fc42982626ce69a0a95d3
musaCODEzz <musamaxwell15@gmail.com>
2025-09-08 20:19:06 +0000

    First commit for calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#This script is suppossed to emulate a calculator
+#Author: Musa Maxwel
+#Date: 8th September 2025
+#Version: v0.1
+
+#
+echo "This script is for emulating a simple calculator"
